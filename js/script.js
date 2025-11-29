1e53ba85a094012e60851f30ba60d7f3012d1fbd
Guido Saez <110474456+GuidoSaez@users.noreply.github.com>
2025-11-29 17:08:01 -0300

    corrección

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 let btnSidebar = document.getElementById("header__btn");
 let sidebar = document.getElementById("sidebar");
 
+sidebar.style.display = "none";
+
 btnSidebar.addEventListener('click', () => {
-  if (sidebar.style.display === "none" || sidebar.style.display === "") {
+  if (sidebar.style.display === "none") {
     sidebar.style.display = "block";
     btnSidebar.innerHTML = '<i class="fa-solid fa-xmark"></i>';
   } else {
